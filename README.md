b52793ca11675896b6671e2bd488383ef81e5dcc
Sumeyra <sumeyra@sumeyra-macbook-pro.home>
2025-04-18 22:55:32 +0200

    v

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Cypress_JavaScript
+
+# npx cypress run  : electron headles browser acmadan ile butun testleri calistirir
+# npx cypress info : bilgisayarda yuklu browser bilgilerini, cypress versionu .. bilgi verir
+# npx cypress verify : yuklu olup olmadigini kotol eder
+
+# npx cypress run -- spec 'cypress/e2e/day02' : istenilen klasordeki tum tesleri calistirma
+# npx cypress run -- spec 'cypress/e2e/day02' -- browser chrome
---
+++ cypress/e2e/0-myCypress/day05/C01_Iframe.cy.js
@@ -0,0 +1,25 @@
+describe('', () => {
+
+    //https://www.npmjs.com/package/cypress-iframe 
+    //Terminalden npm install -D cypress-iframe komutu ile kurulum
+    //require('cypress-iframe') kodunu e2e.js dosyasina ekledik
+    //Bu satirla biz cy.frameLoaded ve cy.iframe komutlarini kullanmamizi saglar
+
+
+    it('IFrame testi', () => {
+        cy.visit('https://the-internet.herokuapp.com/iframe')
+        //driver.switchTo.frame(locate)
+
+        cy.frameLoaded('#mce_0_ifr')
+        cy.iframe().find('p').clear()
+        cy.iframe().find('p').type('Have a WISE Day')
+        cy.log('Sayfa girisi dogrulandi')
+        cy.screenshot()
+    });
+
+
+    it('', () => {
+        
+    });
+    
+});
